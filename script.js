09801f144514498567a07b36142723de50d5399b
Thraxx <165853102+JovemThraxx@users.noreply.github.com>
2024-04-13 23:17:30 -0300

    número de celular alterado

--- script.js
+++ script.js
@@ -161,7 +161,7 @@ const cartItems = cart.map((item) => {
 }).join("")
 
 const message = encodeURIComponent(cartItems)
-const phone = "11-939409965"
+const phone = "+55 11 939409965"
 
 window.open(`https://wa.me/${phone}?text=${message} Endereço: ${addressInput.value}, "_blank"`)
 
